29226d5e9d94d9a2aa04daa0e6732ecef9ddcb74
WINMAC\coral <coralchen1105@gmail.com>
2017-03-19 22:23:33 +1100

    ini Calculator

---
+++ app/src/main/java/com/example/coral/calculator/MainActivity.java
@@ -0,0 +1,106 @@
+package com.example.coral.calculator;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.databinding.DataBindingUtil;
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    ArrayList<Button> numberList = new ArrayList<Button>();
+
+    private double valueOne = Double.NaN;
+    private double valueTwo;
+    private String valueOneText;
+    private String valueTwoText;
+    private String operator = "";
+    private TextView displayNumber;
+
+    private Button one;
+    private Button two;
+    private Button three;
+    private Button four;
+    private Button five;
+    private Button six;
+    private Button seven;
+    private Button eight;
+    private Button nine;
+    private Button ze;
+    private TextView numberDisplay;
+    private Button plus;
+    private Button minus;
+    private Button divide;
+    private Button mult;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        one = (Button) findViewById(R.id.one);numberList.add(one);
+        two = one = (Button) findViewById(R.id.two);
+        three = (Button) findViewById(R.id.three);
+        four = (Button) findViewById(R.id.four);
+        five = (Button) findViewById(R.id.five);
+        six = (Button) findViewById(R.id.six);
+        seven = (Button) findViewById(R.id.seven);
+        eight = (Button) findViewById(R.id.eight);
+        nine = (Button) findViewById(R.id.nine);
+        ze = (Button) findViewById(R.id.ze);
+        plus = (Button) findViewById(R.id.plus);
+
+
+
+
+    }
+
+    public void iniNumber(View v){
+        Button b = (Button) v;
+        double buttonNumber = Integer.parseInt(b.getText().toString());
+
+
+        if(operator == ""){
+            valueOne += buttonNumber;
+        }else{
+            valueTwo += buttonNumber;
+        }
+    }
+
+    public void iniOperator(View v){
+        Button b = (Button) v;
+        operator = b.getText().toString();
+
+    }
+
+    public void mathOperate(View v) {
+
+
+        switch (operator){
+            case "+": Maths.add(valueOne,valueTwo);
+            case "-": Maths.sub(valueOne,valueTwo);
+            case "*": Maths.mult(valueOne,valueTwo);
+            case "/": Maths.div(valueOne,valueTwo);
+        }
+    }
+
+
+
+}
---
+++ app/src/main/java/com/example/coral/calculator/Maths.java
@@ -0,0 +1,29 @@
+package com.example.coral.calculator;
+
+/**
+ * Created by coral on 18/03/2017.
+ */
+
+public class Maths {
+
+    public static double add(double a, double b){
+        return a+b;
+
+    }
+
+    public static double sub(double a, double b){
+        return a-b;
+
+    }
+
+    public static double mult(double a, double b){
+        return a*b;
+
+    }
+
+    public static double div(double a, double b){
+        return a/b;
+
+    }
+
+}
